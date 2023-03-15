3a45742844bde123a10e0042a74e5f1fb7b7163d
Shinya-Fukuta0116 <alleyezon.djwanted@gmail.com>
2023-03-16 06:42:09 +0900

    fix writeInput

--- src/pages/Write/Write.jsx
+++ src/pages/Write/Write.jsx
@@ -9,10 +9,10 @@ export default function Write() {
             <i className="writeIcon fa-regular fa-plus"></i>
           </label>
           <input type="file" id='fileInput' style={{ display: "none" }} />
-          <input type="text" placeholder='Title' className='witeInput' autoFocus={true} />
+          <input type="text" placeholder='Title' className='writeInput' autoFocus={true} />
         </div>
         <div className="writeFormGroup">
-          <textarea placeholder='Tell your story...' type="text" className='wrtieInput witeText'></textarea>
+          <textarea placeholder='Tell your story...' type="text" className='writeInput writeText'></textarea>
         </div>
         <button className="writeSubmit">Publish</button>
       </form>
