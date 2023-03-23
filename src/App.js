b06e0c82eee4f1e3fcb99f1a3cdafba8ea13e92d
Shinya-Fukuta0116 <alleyezon.djwanted@gmail.com>
2023-03-24 05:26:13 +0900

    add routes and register oage

--- src/App.js
+++ src/App.js
@@ -6,15 +6,24 @@ import Single from './pages/single/Single';
 import Write from './pages/Write/Write';
 import Settings from './pages/settings/Settings';
 import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 
 
 
 function App() {
   return (
-    <>
+    <Router>
       <TopBar />
-      <Login />
-    </>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/write' element={<Write />} />
+        <Route path='/settings' element={<Settings />} />
+        <Route path='/post/:postId' element={<Single />} />
+      </Routes>
+    </Router>
   );
 }
 
