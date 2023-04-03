b5edf7b1e8b891b82570eed090242702ec47d2be
Shinya-Fukuta0116 <alleyezon.djwanted@gmail.com>
2023-04-04 05:41:55 +0900

    add link pn calendar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,34 @@
 import './sidebar.css';
 import Image from './aboutMe.png'
 import Calendar from 'react-calendar';
+import BlackImage from './Black.png';
 import 'react-calendar/dist/Calendar.css';
+import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
-export default function sidebar() {
+export default function Sidebar() {
+  const [showImage, setShowImage] = useState(false); // useStateを正しく使う
+  const tileContent = ({ date, view }) => {
+    if (date.getDate() === 4 && date.getMonth() === 3) {
+      return (
+        <div className='sidebarCalendar'
+          onMouseEnter={() => setShowImage(true)} // ホバーしたら画像を表示
+          onMouseLeave={() => setShowImage(false)} // ホバーが外れたら画像を非表示
+          style={{ maxWidth: '600px' }}
+        >
+          <Link to={`/calendar/${date.toISOString().slice(0, 10)}`}>
+            <p style={{ fontSize: '10px' }}>Currensy</p>
+          </Link>
+          {showImage && ( // showImageがtrueの場合に画像を表示
+            <img src={BlackImage} alt="Currency" style={{ width: '100%', height: 'auto' }} />
+          )}
+        </div>
+      );
+    } else {
+      return null;
+    }
+  };
+
   return (
     <div className='sidebar'>
       <div className="sidebarItem">
@@ -23,7 +48,7 @@ export default function sidebar() {
       <div className="sidebarItem">
         <span className="sidebarTitle">CALENDAR</span>
         <div className="sidebarCalendar">
-          <Calendar />
+          <Calendar locale="en-US" tileContent={tileContent} />
         </div>
       </div>
       <div className="sidebarItem">
